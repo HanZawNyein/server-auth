fb3eee1d7be11a154f54dbd08ba4b4c09acd943f
hanzawnyein <hanzawnyineonline@gmail.com>
2024-10-31 14:08:31 +0630

    [FIX] auth_session_timeout : fixing unit test clear cache

--- auth_session_timeout/tests/test_ir_config_parameter.py
+++ auth_session_timeout/tests/test_ir_config_parameter.py
@@ -23,23 +23,3 @@ class TestIrConfigParameter(common.TransactionCase):
     def test_check_session_param_urls(self):
         urls = self.param_obj._auth_timeout_get_parameter_ignored_urls()
         self.assertIsInstance(urls, list)
-
-
-class TestIrConfigParameterCaching(common.TransactionCase):
-    def setUp(self):
-        super(TestIrConfigParameterCaching, self).setUp()
-        self.db = self.env.cr.dbname
-        self.param_obj = self.env["ir.config_parameter"]
-        self.get_param_called = False
-        test = self
-
-        def get_param(*args, **kwargs):
-            test.get_param_called = True
-            return orig_get_param(*args[1:], **kwargs)
-
-        orig_get_param = self.param_obj.get_param
-        self.param_obj._patch_method("get_param", get_param)
-
-    def tearDown(self):
-        super(TestIrConfigParameterCaching, self).tearDown()
-        self.param_obj._revert_method("get_param")
